d10032c5b43e6cff60063ec747433f43d6ed4660
franarena <93073355+franarena@users.noreply.github.com>
2026-02-15 16:23:04 +0100

    ritocca card e aggiunge le prime immagini

--- src/app/car-list/car.ts
+++ src/app/car-list/car.ts
@@ -1,7 +1,10 @@
 export interface Car {
   brand: string;
   model: string;
+  color?: string; // Campo opzionale
   imageUrl: string;
   price: number;
+  owner: string;
+  plate: string;
   daysSinceLastPayment: number; // Nuovo campo per i giorni dall'ultimo pagamento
 }
