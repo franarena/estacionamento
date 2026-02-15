44c7c45cf6f884da5f0f3ec3bd832d57ad1f82e0
franarena <93073355+franarena@users.noreply.github.com>
2026-02-15 21:18:58 +0100

    effettua correzioni grafiche

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -11,11 +11,11 @@ export class DataService implements InMemoryDbService {
 
   createDb() {
     const cars= [
-      { brand: 'Tesla', model: 'Model 3', color: 'Nero', owner: 'Mario Rossi', plate: 'AB123CD', imageUrl: 'assets/tesla.png', daysSinceLastPayment: 15 },
-      { brand: 'BMW', model: 'i4', color: 'Bianco', owner: 'Giuseppe Verdi', plate: 'EF456GH', imageUrl: 'assets/bmw.jpg', daysSinceLastPayment: 20 },
-      { brand: 'Audi', model: 'Q4 e-tron', color: 'Grigio', owner: 'Anna Bianchi', plate: 'IJ789KL', imageUrl: 'assets/audi.png', daysSinceLastPayment: 25 },
-      { brand: 'Fiat', model: '500e', color: 'Rosso', owner: 'Luca Neri', plate: 'MN012OP', imageUrl: 'assets/fiat.jpg', daysSinceLastPayment: 10 }, 
-      { brand: 'Geely', model: 'EX2', color:'Blu' , owner: 'Francesca Gialli', plate: 'QR345ST', imageUrl: 'assets/geely.png', daysSinceLastPayment: 30 }
+      { brand: 'Tesla', model: 'Model 3', color: 'Nero', owner: 'Mario Rossi', plate: 'AB123CD', imageUrl: null, daysSinceLastPayment: 15 },
+      { brand: 'BMW', model: 'i4', color: 'Bianco', owner: 'Giuseppe Verdi', plate: 'EF456GH', imageUrl: null, daysSinceLastPayment: 20 },
+      { brand: 'Audi', model: 'Q4 e-tron', color: 'Grigio', owner: 'Anna Bianchi', plate: 'IJ789KL', imageUrl: null, daysSinceLastPayment: 25 },
+      { brand: 'Fiat', model: '500e', color: 'Rosso', owner: 'Luca Neri', plate: 'MN012OP', imageUrl: null, daysSinceLastPayment: 10 }, 
+      { brand: 'Geely', model: 'EX2', color:'Blu' , owner: 'Francesca Gialli', plate: 'QR345ST', imageUrl: null, daysSinceLastPayment: 30 }
     ];
     return { cars }; // L'URL della API sarà 'api/cars'
   }  
